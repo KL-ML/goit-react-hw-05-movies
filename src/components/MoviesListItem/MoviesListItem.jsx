15992380dda4efc34bc9e2476dffa7211b59e9c5
Natalya Karpova <knataly0513@gmail.com>
2023-01-27 18:31:25 +0200

    Update MoviesListItem.jsx

--- src/components/MoviesListItem/MoviesListItem.jsx
+++ src/components/MoviesListItem/MoviesListItem.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const MoviesListItem = ({ movie }) => {
     return (
         <li key={movie.id}>
-            <Link>{movie.title}</Link>
+            <Link to={`${movie.id}`}>{movie.title}</Link>
         </li>
     );
 }
