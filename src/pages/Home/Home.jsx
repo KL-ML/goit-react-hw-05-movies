3b6337e424036935a50eca645ab6f85500f01749
Natalya Karpova <knataly0513@gmail.com>
2023-01-27 10:43:56 +0200

    Create Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+//Тут робимо запит на бекенд для отримання списку трендів
+
+
+
+const Home = () => {
+    return (
+        <>
+            <h2>Trending today</h2>
+            <ul>
+                <li>Movie 1</li>
+                <li>Movie 2</li>
+                <li>Movie 3</li>
+                <li>Movie 4</li>
+                <li>Movie 5</li>
+                <li>Movie 6</li>
+                <li>Movie 7</li>
+                <li>Movie 8</li>
+                <li>Movie 9</li>
+                <li>Movie 10</li>
+            </ul> 
+        </>
+    );
+};
+
+export default Home;
