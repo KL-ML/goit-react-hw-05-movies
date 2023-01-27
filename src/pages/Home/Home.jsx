9b67f1dffbb9d88d95ff2aeb7398827bed808150
Natalya Karpova <knataly0513@gmail.com>
2023-01-27 15:25:06 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,23 +1,34 @@
-import React from "react";
-//Тут робимо запит на бекенд для отримання списку трендів
-
-
+import React, { useEffect, useState } from "react";
+import { getMovies } from "services/movies.api";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+    // const [loading, setLoading] = useState(false);
+    const [movies, setMovies] = useState(null);
+    const trending = '/trending/movie/day';
+    useEffect(() => {
+        const fetchData = async (params, purpose) => {
+            // setLoading(true);
+            try {
+                const data = await getMovies(params, purpose);
+                setMovies(data.results);
+                console.log(data.results);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData({}, trending);
+    }, []);
+
     return (
         <>
             <h2>Trending today</h2>
             <ul>
-                <li>Movie 1</li>
-                <li>Movie 2</li>
-                <li>Movie 3</li>
-                <li>Movie 4</li>
-                <li>Movie 5</li>
-                <li>Movie 6</li>
-                <li>Movie 7</li>
-                <li>Movie 8</li>
-                <li>Movie 9</li>
-                <li>Movie 10</li>
+                {movies?.map(movie => (
+                    <li key={movie.id}>
+                        <Link>{movie.original_title}</Link>
+                    </li>
+                ))}
             </ul> 
         </>
     );
