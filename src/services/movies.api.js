f15732978c088b90eb1b2b82c05d739d3ba7c402
Natalya Karpova <knataly0513@gmail.com>
2023-01-27 10:44:15 +0200

    Create movies.api.js

---
+++ src/services/movies.api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import { BASE_URL, MY_KEY } from 'constants';
+
+const moviesApi = axios.create({
+    baseURL: { BASE_URL },
+});
+
+export const getMovies = async (params = {}, purpose='') => {
+  const { data } = await moviesApi.get(purpose, {
+    params: {
+      api_key: MY_KEY,
+      ...params,
+    },
+  });
+
+  return data;
+};
