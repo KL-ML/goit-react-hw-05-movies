965080b951b4b26b80477c525c2ce1845acb3991
Natalya Karpova <knataly0513@gmail.com>
2023-01-28 22:38:32 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,19 @@ import Movies from '../pages/Movies/Movies';
 import { Routes, Route } from 'react-router-dom';
 import { Sidebar } from './Header/Sidebar';
 import { MovieDetails } from 'pages/MovieDetails';
+import NotFound from 'pages/NotFound/NotFound';
 
 
 export const App = () => {
   return (
     <>
-    <Sidebar />
-        <Routes>
-          <Route path="" element={<Home />}/>
+      <Sidebar />
+      <Routes>
+        <Route path="" element={<Home />} />
         <Route path='movies' element={<Movies />} />
-        <Route path='movies/:movieId' element={<MovieDetails />} />
-        </Routes>
+        <Route path='/movies/:movieId' element={<MovieDetails />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </>
   );
 };
