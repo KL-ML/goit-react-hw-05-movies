c782cf8b2b97f38323e4c977cc8abf6f42536649
Natalya Karpova <knataly0513@gmail.com>
2023-01-28 22:38:36 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,30 +1,58 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { getMovies } from '../../services/movies.api';
 // import { Link } from "react-router-dom";
 
 export const MovieDetails = () => {
+
+    const [movie, setMovie] = useState(null);
+    const { movieId } = useParams();
+    const movieUrl = `/movie/${movieId}`;
+    
+
+    useEffect(() => {
+        const fetchData = async (params, purpose) => {
+            // setLoading(true);
+            try {
+                const data = await getMovies(params, purpose);
+                setMovie(data);
+                console.log('data:', data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchData({ append_to_response: 'images' }, movieUrl);
+        
+    }, [movieUrl]);
+
+    console.log('movie:', movie);
     return (
         <>
-            {/* <Link to="movies">Go back</Link> */}
-            <div className="filmCard">
-                <div className="imgThumb">
-                    <img src='' alt='' />
-                </div>
-                <div className="filmInfo">
-                    <h2>Movie name</h2>
-                    <p>User Score: 75%</p>
-                    <h3>Overview</h3>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam, dolorem repellat eius quasi molestias iure. Ab nihil non aut sint reiciendis recusandae, alias architecto dolore fugiat repellendus doloribus laudantium provident.</p>
-                    <h4>Genres</h4>
-                    <p>Drama, History, War</p>
-                </div>
-            </div>
-            <div className="additionalInfo">
-                <p className="additionalTitle">Additional information</p>
-                <ul>
-                    <li>Cast</li>
-                    <li>Reviews</li>
-                </ul>
-            </div>
+            {movie &&
+                <div className="filmCard">
+                    <div className="imgThumb">
+                        <img src='' alt='' />
+                    </div>
+                    <div className="filmInfo">
+                        <h2>{movie.title}</h2>
+                        <p>User Score: {movie.popularity}%</p>
+                        <h3>Overview</h3>
+                        <p>{movie.overview}</p>
+                        <h4>Genres</h4>
+                        <ul>
+                            {movie.genres.map(genre => {
+                                return <li key={genre.id}>{genre.name}</li>
+                            })}
+                        </ul>
+                    </div>
+                    <div className="additionalInfo">
+                        <p className="additionalTitle">Additional information</p>
+                        <ul>
+                            <li>Cast</li>
+                            <li>Reviews</li>
+                        </ul>
+                    </div>
+                </div>}
         </>
     );
 };
