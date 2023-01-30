8664599a9b52209634f961b615ba038bcf21cd91
Natalya Karpova <knataly0513@gmail.com>
2023-01-31 00:11:05 +0200

    Update Movies.jsx

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,43 +1,51 @@
 import { MoviesListItem } from "components/MoviesListItem";
 import { SearchForm } from "components/SearchForm";
-import React, {useEffect, useState} from "react";
-
+import React, { useEffect, useState } from "react";
+import { useSearchParams } from 'react-router-dom';
+import css from './Movies.module.css';
 import {getMovies} from '../../services/movies.api'
+import { toast } from "react-toastify";
 
 const Movies = () => {
     const [movies, setMovies] = useState(null);
-    const [searchQuery, setSerchQuery] = useState('')
-    const search = '/search/movie';
+    const purpose = '/search/movie';
 
+    const [searchParams, setSearchParams] = useSearchParams();
+    const searchQueryUrl = searchParams.get('query') ?? '';
+    
     const handleFormSubmit = searchQuery => {
-        setSerchQuery(searchQuery);
-  }
+        setSearchParams({ query: searchQuery });
+    }
 
     useEffect(() => {
-        if (!searchQuery) {
+        if (!searchQueryUrl) {
             return;
         }
         const fetchData = async (params, purpose) => {
             // setLoading(true);
             try {
                 const data = await getMovies(params, purpose);
+                if (data.total_results === 0) {
+                    toast.error('There are no movies on your query!');
+                    return;
+                }
                 setMovies(data.results);
             } catch (error) {
-                console.log(error);
+                toast.error(error);
             }
         };
-        fetchData({query: searchQuery}, search);
-    }, [searchQuery]);
+        fetchData({query: searchQueryUrl}, purpose);
+    }, [searchQueryUrl]);
    
     return (
-        <>
-            <SearchForm onSubmit={handleFormSubmit}/>
+        <div className={css.moviesPage}>
+            <SearchForm onSubmit={handleFormSubmit} searchQueryUrl={searchQueryUrl}/>
             <ul>
                 {movies?.map(movie => (
                     <MoviesListItem key={movie.id} movie={movie}/>
                 ))}
             </ul>
-        </>
+        </div>
     )
 };
 
