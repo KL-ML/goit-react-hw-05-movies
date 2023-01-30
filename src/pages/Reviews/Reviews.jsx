cbae6973a3f210f2ccf4e2e80f4ee12249c74974
Natalya Karpova <knataly0513@gmail.com>
2023-01-31 00:11:11 +0200

    Delete Reviews.jsx

--- src/pages/Reviews/Reviews.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export const Reviews = () => {
-    return <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Amet natus dignissimos eos magnam cupiditate similique quaerat illum voluptate et odit. Sapiente ipsum ut dignissimos reiciendis cumque id soluta in cupiditate? Lorem ipsum dolor sit amet consectetur adipisicing elit. Error cupiditate fugiat tenetur minus qui repudiandae fugit dolorum vitae, voluptas quo aut ea corrupti aliquam enim. Cum natus porro delectus aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa dignissimos dicta illo optio veritatis, pariatur neque facilis? Quae fuga, tempore, odit, aperiam rem recusandae maxime ipsa voluptates possimus impedit vitae.</div>
-};
