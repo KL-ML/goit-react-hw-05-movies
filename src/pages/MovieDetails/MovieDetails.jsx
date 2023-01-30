a1c16d982be668110e4343fcac635e9ccfcd1daa
Natalya Karpova <knataly0513@gmail.com>
2023-01-31 00:10:57 +0200

    Update MovieDetails.jsx

--- src/pages/MovieDetails/MovieDetails.jsx
+++ src/pages/MovieDetails/MovieDetails.jsx
@@ -1,14 +1,15 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { Link, Outlet, useLocation, useParams } from "react-router-dom";
 import { getMovies } from '../../services/movies.api';
-// import { Link } from "react-router-dom";
+import css from './MovieDetails.module.css';
+import { toast } from "react-toastify";
 
 export const MovieDetails = () => {
 
-    const [movie, setMovie] = useState(null);
+    const [movie, setMovie] = useState({});
     const { movieId } = useParams();
     const movieUrl = `/movie/${movieId}`;
-    
+    const location = useLocation();
 
     useEffect(() => {
         const fetchData = async (params, purpose) => {
@@ -16,43 +17,62 @@ export const MovieDetails = () => {
             try {
                 const data = await getMovies(params, purpose);
                 setMovie(data);
-                console.log('data:', data);
             } catch (error) {
-                console.log(error);
+                toast.error(error);
             }
         };
-        fetchData({ append_to_response: 'images' }, movieUrl);
-        
+        fetchData({}, movieUrl);
     }, [movieUrl]);
 
-    console.log('movie:', movie);
+    const { poster_path, title, vote_average, overview, genres } = movie;
     return (
         <>
+            <Link to={location.state?.from ?? '/'}>Go back</Link>
             {movie &&
-                <div className="filmCard">
-                    <div className="imgThumb">
-                        <img src='' alt='' />
+                <>
+                    <div className={css.filmCard}>
+                        <div className="imgThumb">
+                            <img src={
+                                poster_path
+                                    ? `https://image.tmdb.org/t/p/w300${poster_path}`
+                                    : `http://www.suryalaya.org/images/no_image.jpg`
+                            }
+                                width={320}
+                                loading="lazy"
+                                alt="poster" />
+                        </div>
+                        <div className="filmInfo">
+                            <h2>{title}</h2>
+                            <p>User Score: {(vote_average * 10).toFixed()}%</p>
+                            <h3>Overview</h3>
+                            <p>{overview}</p>
+                            <h4>Genres</h4>
+                            <ul className={css.genresList}>
+                                {genres &&
+                                    genres.length &&
+                                    genres.map(({ id, name }) => {
+                                        return <li key={id}>{name}</li>
+                                    })}
+                            </ul>
+                        </div>
+                    
                     </div>
-                    <div className="filmInfo">
-                        <h2>{movie.title}</h2>
-                        <p>User Score: {movie.popularity}%</p>
-                        <h3>Overview</h3>
-                        <p>{movie.overview}</p>
-                        <h4>Genres</h4>
+                    <div className={css.additionalInfo}>
+                        <h4 className="additionalTitle">Additional information</h4>
                         <ul>
-                            {movie.genres.map(genre => {
-                                return <li key={genre.id}>{genre.name}</li>
-                            })}
+                            <li>
+                                <Link to="cast" state={{ from: location.state?.from }}>Cast</Link>
+                            </li>
+                            <li>
+                                <Link to="reviews" state={{ from: location.state?.from }}>Reviews</Link>
+                            </li>
                         </ul>
                     </div>
-                    <div className="additionalInfo">
-                        <p className="additionalTitle">Additional information</p>
-                        <ul>
-                            <li>Cast</li>
-                            <li>Reviews</li>
-                        </ul>
+                    <div>
+                        <Outlet />
                     </div>
-                </div>}
+                </>
+            }
         </>
     );
 };
