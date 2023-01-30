b5fdc3abf095db9610172b60c77f750ca61a1940
Natalya Karpova <knataly0513@gmail.com>
2023-01-31 00:10:41 +0200

    Update MoviesListItem.jsx

--- src/components/MoviesListItem/MoviesListItem.jsx
+++ src/components/MoviesListItem/MoviesListItem.jsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export const MoviesListItem = ({ movie }) => {
+    const location = useLocation();
+
     return (
         <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+            <Link
+                to={`/movies/${movie.id}`}
+                state={{from: location}}
+            >
+                {movie.title}
+            </Link>
         </li>
     );
 }
