dd6997b2118071243cf538b9e686a2c28a3765d4
Natalya Karpova <knataly0513@gmail.com>
2023-01-31 11:38:57 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,34 @@
+import { lazy, Suspense } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-import Home from '../pages/Home/Home';
-import Movies from '../pages/Movies/Movies';
-import { Routes, Route } from 'react-router-dom';
 import { Sidebar } from './Header/Sidebar';
-import { MovieDetails } from 'pages/MovieDetails';
-import NotFound from 'pages/NotFound/NotFound';
-import { Cast } from 'pages/MovieDetails/Cast';
-import { Reviews } from 'pages/MovieDetails/Reviews';
 
+const Home = lazy(() => import('../pages/Home/Home'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('pages/MovieDetails/Cast/Cast'));
+const Reviews = lazy(() => import('pages/MovieDetails/Reviews/Reviews'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 export const App = () => {
   return (
     <>
       <Sidebar />
-      <Routes>
-        <Route path="" element={<Home />} />
-        <Route path='movies' element={<Movies />} />
-        <Route path='/movies/:movieId' element={<MovieDetails />}>
-          <Route path="cast" element={<Cast />} />
-          <Route path="reviews" element={<Reviews />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes>
+          <Route path="" element={<Home />} />
+          <Route path='movies' element={<Movies />} />
+          <Route path='/movies/:movieId' element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
       <ToastContainer />
-      </>
+    </>
   );
 };
-
-//Main Page
-//-Header
-//-Tranding today
-// ..list of films
-//--Film card 
-//---Cast list
-//---Reviews
